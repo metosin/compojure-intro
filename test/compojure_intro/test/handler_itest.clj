9c84e3e39dd1d4f6e8e8230d2d59cdca5fed7127
Tommi Reiman <tommi.reiman@metosin.fi>
2013-10-07 11:10:03 +0300

    running ring in non-dev mode

--- test/compojure_intro/test/handler_itest.clj
+++ test/compojure_intro/test/handler_itest.clj
@@ -5,6 +5,7 @@
 (def base-url "http://localhost:3000")
 
 (comment
+  ;; export LEIN_NO_DEV=false; lein ring server
   (facts "about apis"
     (fact "v1 ping works"
       (let [{:keys [status body]} (c/get (str base-url "/api/v1/ping"))]
